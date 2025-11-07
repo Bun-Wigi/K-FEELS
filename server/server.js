49986fd894224a0d23d2d16f1d149662dc5df600
Trish OMeara Gillespie <trishedith@gmail.com>
2025-11-06 21:34:35 -0500

    server seems running

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5172;
+const PORT = process.env.PORT || 5173;
 const MONGODB_URI =
   "mongodb+srv://guesooul_db_user:0NYKzxnGMcYZWRHO@dramadb.e9dlbow.mongodb.net/?appName=DramaDB";
 
