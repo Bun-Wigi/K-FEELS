084067510bdea59e830c18f228fe2084187017cb
sinsanitized <antsin2@gmail.com>
2025-11-11 22:49:34 -0500

    tuesday refactor

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,21 +1,18 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import { store } from './store'
-import App from './App'
-import './index.css'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store";
+import App from "./App";
+import "./index.css";
 
-const container = document.getElementById('root')
-if (!container) throw new Error('Root element not found')
+const container = document.getElementById("root");
+if (!container) throw new Error("Root element not found");
 
 createRoot(container).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
-)
-
+);
